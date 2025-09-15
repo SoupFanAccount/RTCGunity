02f7f9bba4b40031804caaaf66193c60f430e95b
Ronja <rglusi22@student.aau.dk>
2025-09-15 22:58:47 +0200

    Lecture 1
    Without the customization exercises

---
+++ RTCGunity/Assets/RayCastManipulator.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class RayCastManipulator : MonoBehaviour
+{
+    public int numRaycasts = 10;
+
+    CreateGrid createGrid;
+    List<GameObject> hitCubes = new List<GameObject>();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        createGrid = GetComponent<CreateGrid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hitCubes.Count == createGrid.grid.Length)
+        {
+            foreach (GameObject cube in hitCubes)
+            {
+                cube.SetActive(true);
+            }
+            hitCubes = new List<GameObject>();
+        }
+
+        Vector3 cubeCenter = createGrid.grid[0].localPosition + new Vector3(createGrid.gridResolution / 2, createGrid.gridResolution / 2, createGrid.gridResolution / 2);
+        for (int i = 0; i < numRaycasts; i++)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cubeCenter, Random.onUnitSphere, out hit, 100))
+            {
+                hit.transform.gameObject.SetActive(false);
+                hitCubes.Add(hit.transform.gameObject);
+            }
+        }
+    }
+}
